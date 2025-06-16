cb06c4d4065303ae2ec3181834e664102f881686
Abdullah ERTÜRK <54866587+abdullah-erturk@users.noreply.github.com>
2025-06-16 13:01:37 +0300

    Update TSF Activation.bat

--- ENG/TSF Activation.bat
+++ ENG/TSF Activation.bat
@@ -57,7 +57,7 @@ if %errorlevel% NEQ 0 (
     powershell -Command "Start-Process -FilePath '%~f0' -Verb RunAs"
     exit /b
 )
-call base64decode
+goto :base64decode
 
 ::===============================================================================================================
 
